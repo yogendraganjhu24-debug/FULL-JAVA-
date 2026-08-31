4695ad8acbe8ac52d170257d556cea06a57e0a39
Yogendra Ganjhu <yogendraganjhu24@gmail.com>
2026-08-31 14:48:15 +0530

    main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    
+    public static void main(String[] args) {
+        // this is comments 
+    
+        System.out.println("Hello, World!");
+    }
+}
+
+add two number 
+AddTwoNumber -->pascal naming convention 
+addTowNumber -->camel naming convention
+
