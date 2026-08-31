4f6779c188c3d27b6b05c49c3df7a84274a55caf
Yogendra Ganjhu <yogendraganjhu24@gmail.com>
2026-08-31 20:19:54 +0530

    variable.java

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable_datatype {
+    public static void main(String[] args) {
+        // this is comments 
+        int a = 10; // integer variable
+        double b = 20.5; // double variable
+        String c = "Hello"; // string variable
+        boolean d = true; // boolean variable
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("String: " + c);
+        System.out.println("Boolean: " + d);
+    }
+}
